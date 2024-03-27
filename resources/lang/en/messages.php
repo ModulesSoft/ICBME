eee8b994be801b4be5ae4719b70e57803e72ec60
Yousef Fatouraee <fatouraee96@gmail.com>
2024-03-26 22:57:49 -0600

    Add language files

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,118 @@
+<?php
+
+return [
+    'intro' => [
+        'button' => 'About The ICBME Conferences',
+        'countdown' => [
+            'date' => 'Nov 30, 2023 00:00:00',
+            'days' => 'Days',
+            'hours' => 'Hours',
+            'minutes' => 'Minutes',
+            'seconds' => 'Seconds'
+        ]
+    ],
+    'about' => [
+        'title' => 'About the ICBME',
+        'where' => 'Where',
+        'when' => 'When',
+        'dates' => 'Important Dates'
+    ],
+    'subscribe' => [
+        'title' => 'Newsletter',
+        'subtitle' => 'Submit your email to let us send you conference news.'
+    ],
+    'speaker' => [
+        'title' => 'Keynote & Invited Speakers',
+        'subtitle' => 'Here are our keynote and invited speakers',
+    ],
+    'schedule' => [
+        'title' => 'CONFERENCE PROGRAM',
+        'subtitle' => 'Here is our conference program',
+    ],
+    'hotels' => [
+        'title' => 'Hotels',
+        'subtitle' => 'Here are some qualified nearby hotels',
+    ],
+    'venue' => [
+        'title' => 'Event Venue',
+        'subtitle' => 'Event venue location info and gallery',
+    ],
+    'FAQ' => [
+        'title' => 'F.A.Q',
+        'subtitle' => '',
+    ],
+    'contact' => [
+        'title' => 'Contact Us',
+        'subtitle' => 'Contact form',
+        'address' => 'Address',
+        'phone' => 'Phone Number',
+        'email' => 'Email',
+        'sent' => 'Your message has been sent. Thank you!',
+        'error1' => 'Please enter at least 4 chars',
+        'error2' => 'Please enter a valid email',
+        'error3' => 'Please enter at least 8 chars of subject',
+        'error4' => 'Please write something for us',
+        'submit' => 'Send Message'
+    ],
+    'committees' => [
+        'title' => 'Committees',
+        'subtitle' => 'Here are ICBME committees',
+    ],
+    'news' => [
+        'title' => 'News',
+        'subtitle' => 'Here you can read our news summary.',
+    ],
+    'gallery' => [
+        'title' => 'Gallery',
+        'subtitle' => 'Check our gallery from the recent events',
+    ],
+    'sponsors' => [
+        'title' => 'Sponsors',
+        'subtitle' => ''
+    ],
+    'authors' => [
+        'title' => 'For Authors',
+        'subtitle' => 'Please read these instruction carefully',
+    ],
+    'footer' => [
+        'links' => [
+            'title' => 'Useful Links',
+            'link1' => 'IEEE Xplore',
+            'link2' => 'IEEE Iran Section',
+            'link3' => 'Biomedical Engineering @ AUT',
+            'link4' => 'Islamic World Science Citation',
+            'link5' => 'Login',
+            'link6' => 'Admin Panel'
+        ],
+        'links2' => [
+            'title' => 'Useful Links',
+            'link1' => 'ISBME',
+            'link2' => 'edas',
+            'link3' => 'Ministry of Science Research and Technology',
+            'link4' => 'Ministry of Health and Medical Education',
+            'link5' => 'Faculty of New Sciences and Technologies, University of Tehran',
+        ],
+        'copyright' => [
+            'row1' => '© Copyright ICBME. All Rights Reserved',
+            'row2' => 'Iranian Society for Biomedical Engineering',
+            'link' => 'https://isbme.org/'
+        ],
+        'contact' => [
+            'title' => 'Contact'
+        ],
+    ],
+    'speaker-details' => [
+        'title' => 'Know more about'
+    ],
+    'workshops' => [
+        'title' => 'Program schedule',
+        'subtitle' => 'Here are our program schedule',
+    ],
+    'poster' => [
+        'title' => 'ICBME 2023 POSTER',
+        'subtitle' => 'Here is our official poster',
+    ],
+    'thesisfestival' => [
+        'title' => "Thesis Festival",
+    ]
+];
