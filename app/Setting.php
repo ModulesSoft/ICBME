c9542e296d47f461f5bae3179b3ecbd6f873590f
Yousef Fatouraee <fatouraee96@gmail.com>
2024-03-27 23:12:49 -0600

    Add translation for value column

--- app/Setting.php
+++ app/Setting.php
@@ -4,12 +4,15 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Translatable\HasTranslations;
 
 class Setting extends Model
 {
     use SoftDeletes;
+    use HasTranslations;
 
     public $table = 'settings';
+    public $translatable = ['value'];
 
     protected $dates = [
         'created_at',
