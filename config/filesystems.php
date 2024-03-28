30a096ae0e5926d4fde76539ba4dd883d7a4f2ce
Yousef Fatouraee <fatouraee96@gmail.com>
2024-03-27 19:20:17 -0600

    Add media disk and change the public disk to the default configuration

--- config/filesystems.php
+++ config/filesystems.php
@@ -51,8 +51,14 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL') . '/img',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
+        ],
+
+        'media' => [
+            'driver' => 'local',
+            'root' => public_path('img'),
+            'url' => env('APP_URL') . '/img',
         ],
 
         's3' => [
