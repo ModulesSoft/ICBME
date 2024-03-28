d7162dbded759ec891b481fad7c5433ef1168f69
Yousef Fatouraee <fatouraee96@gmail.com>
2024-03-28 17:23:38 -0600

    Add setLocale middleware

--- app/Http/Middleware/SetLocale.php
+++ app/Http/Middleware/SetLocale.php
@@ -3,22 +3,32 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Session;
 
 class SetLocale
 {
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next)
     {
-        if (request('change_language')) {
-            session()->put('language', request('change_language'));
-            $language = request('change_language');
-        } elseif (session('language')) {
-            $language = session('language');
-        } elseif (config('panel.primary_language')) {
-            $language = config('panel.primary_language');
+        if (
+            isset(auth()->user()->language)
+            && auth()->user()->language
+            && array_key_exists(auth()->user()->language, Config::get('languages'))
+        ) {
+            if (session()->has('applocale')) {
+                App::setLocale(session()->get('applocale'));
+            } else {
+                Session::put('applocale', auth()->user()->language);
+            }
         }
-
-        if (isset($language)) {
-            app()->setLocale($language);
+        if (session()->has('applocale')) {
+            App::setLocale(session()->get('applocale'));
+        } else {
+            // This is optional as Laravel will automatically set the fallback language if there is
+            // none specified
+            App::setLocale(config('app.fallback_locale'));
         }
 
         return $next($request);
