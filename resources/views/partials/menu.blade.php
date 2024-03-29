c8a58b548a162486bb351d32e719ffca7e57a64c
Yousef Fatouraee <fatouraee96@gmail.com>
2024-03-29 09:20:55 -0600

    Add home link

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -1,6 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4" style="min-height: 917px; {{ __('global.dir') === 'rtl' ? 'right: 0;' : '' }}">
     <!-- Brand Logo -->
-    <a href="#" class="brand-link">
+    <a href="/" class="brand-link">
         <span class="brand-text font-weight-light">{{ trans('panel.site_title') }}</span>
     </a>
 
