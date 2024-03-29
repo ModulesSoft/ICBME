274587e400279edd5e81f9792515e03a5f373455
Yousef Fatouraee <fatouraee96@gmail.com>
2024-03-28 22:25:06 -0600

    Add SetYear middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -22,6 +22,7 @@ class Kernel extends HttpKernel
         ],
         'web' => [
             \App\Http\Middleware\Archive::class,
+            \App\Http\Middleware\SetYear::class,
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
