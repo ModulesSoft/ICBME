d3fb152d043510eb3d1b53958dfb4ab178a17eac
Yousef Fatouraee <fatouraee96@gmail.com>
2024-03-29 17:57:23 -0600

    Change hotel description to markup text

--- resources/views/sections/hotels.blade.php
+++ resources/views/sections/hotels.blade.php
@@ -8,20 +8,20 @@
 
     <div class="row">
       @foreach($hotels as $hotel)
-        <div class="col-lg-4 col-md-6">
-          <div class="hotel">
-            <div class="hotel-img">
-              <img src="{{ $hotel->photo->getUrl() }}" alt="{{ $hotel->name }}" class="img-fluid">
-            </div>
-            <h3><a href="{{ $hotel->link }}">{{ $hotel->name }}</a></h3>
-            <div class="stars">
-              @for($i = 0; $i < $hotel->rating; $i++)
-                <i class="fa fa-star"></i>
-              @endfor
-            </div>
-            <p>{{ $hotel->description }}</p>
+      <div class="col-lg-4 col-md-6">
+        <div class="hotel">
+          <div class="hotel-img">
+            <img src="{{ $hotel->photo->getUrl() }}" alt="{{ $hotel->name }}" class="img-fluid">
           </div>
+          <h3><a href="{{ $hotel->link }}">{{ $hotel->name }}</a></h3>
+          <div class="stars">
+            @for($i = 0; $i < $hotel->rating; $i++)
+              <i class="fa fa-star"></i>
+              @endfor
+          </div>
+          <p>{!! $hotel->description !!}</p>
         </div>
+      </div>
       @endforeach
     </div>
   </div>
