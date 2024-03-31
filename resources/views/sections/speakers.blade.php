8f0f2f6284e754dcc410303aa65c490ffbbd21a1
Yousef Fatouraee <fatouraee96@gmail.com>
2024-03-31 09:45:36 -0600

    Fix some styling issues

--- resources/views/sections/speakers.blade.php
+++ resources/views/sections/speakers.blade.php
@@ -7,28 +7,28 @@
 
     <div class="row">
       @foreach($speakers as $speaker)
-        <div class="col-lg-3 col-md-6">
-          <div class="speaker">
-            <a href="{{ route('speaker', $speaker->id) }}">
+      <div class="col-lg-3 col-md-6">
+        <div class="speaker">
+          <a href="{{ route('speaker', $speaker->id) }}">
             <img src="{{ $speaker->photo ? $speaker->photo->getUrl('bigthumb') :'' }}" alt="{{ $speaker->name }}">
             <div class="details">
               <h3><a href="{{ route('speaker', $speaker->id) }}">{{ $speaker->name }}</a></h3>
-              <p>{!! $speaker->description !!}</p>
+              <div style="padding: 0 1rem;">{!! $speaker->description !!}</div>
               <div class="social">
                 @if($speaker->twitter)
-                  <a href="{{ $speaker->twitter }}"><img style="height: 20px;border-radius: 50%" src="{{asset('img/googlescholaricon.png')}}"></a>
+                <a href="{{ $speaker->twitter }}"><img style="height: 20px;width: 20px;border-radius: 50%" src="{{asset('img/googlescholaricon.png')}}"></a>
                 @endif
                 @if($speaker->facebook)
-                  <a href="{{ $speaker->facebook }}"><img style="height: 20px;border-radius: 50%" src="{{asset('img/researchgateicon.png')}}"></a>
+                <a href="{{ $speaker->facebook }}"><img style="height: 20px;width: 20px;border-radius: 50%" src="{{asset('img/researchgateicon.png')}}"></a>
                 @endif
                 @if($speaker->linkedin)
-                  <a href="{{ $speaker->linkedin }}"><img style="height: 20px;border-radius: 50%" src="{{asset('img/pubmedicon.png')}}"></a>
+                <a href="{{ $speaker->linkedin }}"><img style="height: 20px;width: 20px;border-radius: 50%" src="{{asset('img/pubmedicon.png')}}"></a>
                 @endif
               </div>
             </div>
-            </a>
-          </div>
+          </a>
         </div>
+      </div>
       @endforeach
     </div>
   </div>
